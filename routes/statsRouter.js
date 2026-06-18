ce0612cf76c2234cb4a621ddc8ee4e699746da9e
Bakhomios Tadres <bakhomytadres@gmail.com>
2026-06-18 23:15:29 +0300

    Add files via upload

--- routes/statsRouter.js
+++ routes/statsRouter.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { getStats, incrementCall } from "../controllers/statsController.js";
-import { verifyToken } from "../middlewares/Auth.js"; 
 
 export const statsRouter = express.Router();
 
-statsRouter.get("/", verifyToken, getStats);
-statsRouter.post("/call", verifyToken, incrementCall);
+statsRouter.get("/", getStats);
+statsRouter.post("/call", incrementCall);
